78795c4af58632ef010a7e2e462ad60553598b8d
Sam Rang <srang2010@gmail.com>
2017-04-21 15:48:18 -0500

    more bootstrapping

--- roles/master/scripts/bootstrap.sh
+++ roles/master/scripts/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cd /opt
 sudo -i
+yum install -y git make rpm-build asciidoc python-setuptools python2-devel
 git clone git://github.com/ansible/ansible.git --recursive
 cd ./ansible
 make rpm
