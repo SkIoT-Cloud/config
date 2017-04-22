fac18ecd1e54f98b3cf2774c950a8736a08acdc7
Sam Rang <srang2010@gmail.com>
2017-04-22 10:36:15 -0500

    progress

---
+++ Dockerfile
@@ -0,0 +1,1 @@
+FROM williamyeh/ansible:centos7
--- roles/master/scripts/bootstrap.sh
+++ roles/master/scripts/bootstrap.sh
@@ -3,9 +3,18 @@ cd /opt
 sudo yum install -y git python2-devel python-setuptools gcc openssl-devel
 sudo easy_install pip
 sudo pip install ansible ansible-vault
-sudo mkdir /app
-chown :wheel /app
+if [ ! -d /app ]; then
+    sudo mkdir /app
+    chown :wheel /app
+fi
 cd /app
-git clone https://github.com/skiot-cloud/config
+if [ ! -d config ]; then
+    git clone https://github.com/skiot-cloud/config
+fi
 cd config
-curl https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py > ec2.py
+if [ ! -f ec2.py ]; then
+    sudo curl https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py > /opt/ec2.py
+    sudo chmod 771 /opt/ec2.py
+    sudo curl https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.ini > /opt/ec2.ini
+fi
+ansible -i ec2.py -m ping
