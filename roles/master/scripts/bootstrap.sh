3554a06f869bb0f4b89bb5c41d652dbc41cfb239
Sam Rang <srang2010@gmail.com>
2017-04-22 09:19:02 -0500

    inventory

--- roles/master/scripts/bootstrap.sh
+++ roles/master/scripts/bootstrap.sh
@@ -3,3 +3,9 @@ cd /opt
 sudo yum install -y git python2-devel python-setuptools gcc openssl-devel
 sudo easy_install pip
 sudo pip install ansible
+sudo mkdir /app
+chown :wheel /app
+cd /app
+git clone https://github.com/skiot-cloud/config
+cd config
+curl https://raw.githubusercontent.com/ansible/ansible/devel/contrib/inventory/ec2.py > ec2.py
