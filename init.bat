6a14b1d9db352476ca2eecf559d0392a14977e6f
Sam Rang <srang2010@gmail.com>
2017-04-23 19:37:27 -0500

    back to where we were hours ago but with inline vault

--- init.bat
+++ init.bat
@@ -1,5 +1,5 @@
 @echo off
 title Bootstraps your AWS environment with ansible
 docker build -t bootstrap-base init
-docker build -t skiot-bootstrap ..
+docker build -t skiot-bootstrap .
 docker run --rm --name=bootstrap skiot-bootstrap
