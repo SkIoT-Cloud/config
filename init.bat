9be5519e70356e19f0184df8d88585bc35dee6dc
Sam Rang <srang2010@gmail.com>
2017-04-23 19:14:57 -0500

    clean stuff up

--- init.bat
+++ init.bat
@@ -1,4 +1,5 @@
 @echo off
 title Bootstraps your AWS environment with ansible
-docker build -t skiot-bootstrap .
+docker build -t bootstrap-base init
+docker build -t skiot-bootstrap ..
 docker run --rm --name=bootstrap skiot-bootstrap
